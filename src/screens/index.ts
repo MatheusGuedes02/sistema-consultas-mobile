72ae49929eb6af2b0df44f1427c3f7d0278442f4
Matheus Guedes Grigorio <matheusguedes02006@gmail.com>
2026-09-10 10:44:30 -0300

    pequeno fix

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,7 +1,2 @@
 export { default as Home } from "./Home";
-import ConsultaCard from "./src/components/ConsultaCard.tsx";
-
-export { default as Home } from "./Home";
 export { default as Admin } from "./Admin";
-
-
